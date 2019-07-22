cd581495fb33ffbcbea281bac10e7ec1ae43cf15
Leo <losoviz@gmail.com>
2019-07-22 12:17:48 +0530

    Extend component to initialize plugin

--- initialize.php
+++ initialize.php
@@ -1,2 +1,17 @@
-<?php 
-require_once 'migrate/pop-queriedobject-wp.php';
+<?php
+$class = (new class() extends \PoP\Root\Component\AbstractComponent
+{
+    /**
+     * Boot component
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        // Initialize code
+        require_once 'migrate/pop-queriedobject-wp.php';  
+    }
+});
+$class::init();
